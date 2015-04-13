26fe9c60b68a58f82854f09263f6dca5b7293c55
benobab <benlacroix83@gmail.com>
2015-04-13 19:36:36 +0200

    CSS added

---
+++ README.txt
@@ -0,0 +1,3 @@
+Dans le fichier index.php, changer le path du fichier style.css 
+Si le projet est à la racine du site : /style.css
+Sinon, lien absolu.
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<body>
+	<head>
+	    <link rel="stylesheet" type="text/css" href="/Web/WebService/xPath/Web-Service---Bonjour-International/style.css">
+	</head>
+	<?php
+	$path = ltrim($_SERVER['REQUEST_URI'], '/');    // Trim leading slash(es)
+	$elements = explode('/', $path); 
+		$XML = simplexml_load_file("bonjour.xml");
+		$result = $XML->xpath('//trad[@langue="'.$elements[count($elements)-1].'"]');
+		if($result)
+		{ ?>
+			<h1>Comment dire bonjour en <?php echo $elements[count($elements)-1]; ?>?</h1>
+			<h2>Pas dur  : bonjour se dit " <?php echo $result[0]; ?>" :-)</h2>
+	<?php	
+		}else { ?>
+			<h1>Web Service - Bonjour </h1>
+			<h2>Il suffit de taper dans l'url /bonjour/XXXXXX</h2>
+			<h3>XXXXX representant la langue dans laquelle vous voulez traduire bonjour ! </h3>
+			<p>
+				Quelques regles a respecter : 
+				<ul>
+					<li>La langue tout en MAJUSCULE</li>
+					<li>Si la langue contient un espace, il faut mettre un tiret du 6  '-'</li>
+				</ul>
+			</p>
+	<?php
+	}
+		
+	?>
+</body>
